b86ee330fc9a5c163c217472386e47b476b7c17d
Agilgeorge <111696090+Agilgeorge@users.noreply.github.com>
2024-08-18 07:46:25 +0530

    just random dumps

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'game_page.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: SnakeGamePage(),
+    );
+  }
+}
